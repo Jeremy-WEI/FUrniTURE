3800e32e0096d94ef0e30b4cf2a688b437231649
JoeJoe1989 <josephzhou2011@gmail.com>
2015-01-17 14:08:57 -0500

    routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,10 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "users/registrations"}
   # devise_for :users
   resources :furnitures
-<<<<<<< HEAD
-  # resources :users
-=======
+
   resources :watchlists do
     resources :furnitures
   end
 
->>>>>>> 1c0315615057eccb1df79ceb2334e5e4eb093628
   # get '/welcome', to: 'application#welcome'
 end
