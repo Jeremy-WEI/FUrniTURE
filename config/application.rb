07cc9e89d7cd8b5617ffefa85358c781bef7844e
Yunchen Wei <weiyunchen0807@outlook.com>
2015-01-17 16:25:28 -0500

    fix google map

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,8 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-require 'openssl'
-require 'geokit'
+# require 'openssl'
+# require 'geokit'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -12,6 +12,7 @@ Bundler.require(*Rails.groups)
 module FUrniTURE
   class Application < Rails::Application
        config.assets.paths << Rails.root.join("app","assets", "fonts")
+       config.assets.paths << Rails.root.join("app","assets", "images")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'application#welcome'
   devise_for :users
   resources :furnitures
+  get '/search', to: 'furnitures#search'
   resources :users
   # get '/welcome', to: 'application#welcome'
 end
