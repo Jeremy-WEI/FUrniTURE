129f0d0b40e37cd6d71537334ff0811ae97e5a3a
Yunchen Wei <weiyunchen0807@outlook.com>
2015-01-16 22:55:57 -0500

    homepage modified

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
-  
+  # before_action :authenticate_user!
+
   def welcome
 
   end
 
   def search
-
+    # cri1 = asdjkl
+    # cri2 = sadjlsa
+    # lat = 1
+    # lon = 1
+    #
+    #
+    #
+    # results = []
+    # render 'furnitures/index'
   end
   
 
