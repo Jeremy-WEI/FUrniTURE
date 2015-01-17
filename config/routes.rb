731a2488b7bdf8c0fce1e64ef181a4296a5857e1
JoeJoe1989 <josephzhou2011@gmail.com>
2015-01-17 14:32:58 -0500

    add to my watchlist

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # devise_for :users
   resources :furnitures
 
+  post 'users/:id' => 'catalog#view'
+
   resources :watchlists do
     resources :furnitures
   end
