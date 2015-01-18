c4d6eab5d3f250f3327a9f4e5a01a4125e4b8498
Yunchen Wei <weiyunchen0807@outlook.com>
2015-01-18 01:17:06 -0500

    bug fixed

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module FUrniTURE
   class Application < Rails::Application
        config.assets.paths << Rails.root.join("app","assets", "fonts")
        config.assets.paths << Rails.root.join("app","assets", "images")
+       config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
