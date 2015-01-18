629972350be445e1389450030b1be186518dc12c
Yunchen Wei <weiyunchen0807@outlook.com>
2015-01-17 21:12:43 -0500

    modified

--- db/schema.rb
+++ db/schema.rb
@@ -30,12 +30,10 @@ ActiveRecord::Schema.define(version: 20150117212744) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.string   "address"
-    t.integer  "watchlist_id"
     t.float    "oldness"
   end
 
   add_index "furnitures", ["user_id"], name: "index_furnitures_on_user_id"
-  add_index "furnitures", ["watchlist_id"], name: "index_furnitures_on_watchlist_id"
 
   create_table "furnitures_watchlists", force: true do |t|
     t.integer "watchlist_id"
